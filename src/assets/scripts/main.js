7a035adbff7e196f1e58d97ae0a2c5300df4b494
Juan Jesús Gutiérrez Ramos <guangle@gmail.com>
2020-12-23 11:34:52 +0100

    primera version con tailwind

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,15 +1,36 @@
-/**
- * Import dependencies from node_modules
- * see commented examples below
+
+
+/** Funcion que se ejecuta en los diferentes test que hay en la web,
+ * se encarga, en funcion de si la respuesta es correcta o no, mostrar
+ * diferentes animaciones en la tarjeta que contiene la pregunta y colorearla
+ * para que el usuario sepa si ha acertado o no
  */
 
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
+var respuesta = function () {
+  var correcta = this.getAttribute("es-correcta");
+  const tarjeta = this.parentElement.parentElement;
 
-/**
- * Write any other JavaScript below
- */
+  tarjeta.classList.remove("shake-horizontal");
+  tarjeta.classList.remove("jello-horizontal");
+  tarjeta.firstElementChild.classList.remove("bg-color_fest_verde");
+  tarjeta.firstElementChild.classList.remove("bg-color_fest_rosa");
+  var respuestas = tarjeta.getElementsByClassName("respuesta");
+  for (var i = 0; i < respuestas.length; i++) {
+    respuestas.item(i).classList.remove("font-weight-bold");
+  }
 
-+( function() {
-  console.log('Hello, UOC!');
-} )();
+  this.classList.add("font-weight-bold");
+  if (correcta == 1) {
+    tarjeta.classList.add("jello-horizontal");
+    tarjeta.firstElementChild.classList.add("bg-color_fest_verde");
+  } else {
+    tarjeta.classList.add("shake-horizontal");
+    tarjeta.firstElementChild.classList.add("bg-color_fest_rosa");
+  }
+};
+
+//Aplicamos la función implementada a cada una de las respuestas
+var respuestas = document.getElementsByClassName("respuesta");
+Array.from(respuestas).forEach(function (r) {
+  r.addEventListener("click", respuesta);
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,39 @@ module.exports = {
     extend: {
       colors: {
         'light-blue': colors.lightBlue,
+        'color_fest_oscuro1' : '#011027',
+        'color_fest_rosa' : '#ff00e6',
+        'color_fest_azul' : '#00b5ec',
+        'color_fest_amarillo' : '#ffeb3b',
+        'color_fest_verde' : '#39ff14',
         cyan: colors.cyan,
       },
     },
+    fontFamily: {
+      trispace: ['Trispace', 'sans-serif'],
+    },
+    //Definimos nuestros propios anchos
+    width: {
+      'full' : '100%',
+      '3/10': '30%',
+      '1/2' : '50%',
+      '1/7': '14.2857143%',
+      '2/7': '28.5714286%',
+      '3/7': '42.8571429%',
+      '4/7': '57.1428571%',
+      '5/7': '71.4285714%',
+      '6/7': '85.7142857%',
+    },
+    //Definimos las sombras propias del estilo de nuestro festival
+    //para poder usar luego las clases generadas por tailwind
+    boxShadow: {
+      'azul' : '0 0 78px 4px rgba(16, 134, 232, 0.73)',
+      'multicolor' : 'inset 0 0 30px #fff, inset 10px 0 40px #f0f, inset -10px 0 40px #0ff, inset 10px 0 150px #f0f, inset -10px 0 150px #0ff, 0 0 30px #fff, -10px 0 40px #f0f, 10px 0 40px #0ff',
+      'gris' : '16px 16px 8px rgba(150, 150, 150, 0.93)'
+    },
+    gridTemplateRows: {
+     '6': 'repeat(5, 100px) 50px',
+    }
   },
   variants: {},
   plugins: [],
