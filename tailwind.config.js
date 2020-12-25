8178ec07a23ff753a76608c557b8d4bc4a6dcd81
Juan Jesús Gutiérrez Ramos <guangle@gmail.com>
2020-12-25 19:25:41 +0100

    v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,6 +34,7 @@ module.exports = {
     //para poder usar luego las clases generadas por tailwind
     boxShadow: {
       'azul' : '0 0 78px 4px rgba(16, 134, 232, 0.73)',
+      'azul-small' : '1px 1px 4px #00b5ec',
       'multicolor' : 'inset 0 0 30px #fff, inset 10px 0 40px #f0f, inset -10px 0 40px #0ff, inset 10px 0 150px #f0f, inset -10px 0 150px #0ff, 0 0 30px #fff, -10px 0 40px #f0f, 10px 0 40px #0ff',
       'gris' : '16px 16px 8px rgba(150, 150, 150, 0.93)'
     },
@@ -46,6 +47,15 @@ module.exports = {
         '25%': { 'box-shadow' : '10px 10px 30px -8px rgba(130, 35, 178, 1)' },
         '50%': { 'box-shadow': '10px 10px 30px -8px rgba(217, 3, 104, 1)' },
         '75%': { 'box-shadow' : '10px 10px 30px -8px rgba(241, 233, 218, 1)' }
+    },
+    spin : {
+      '0%' :  { 'transform' : 'rotate(0deg)' } , 
+      '100%' : { 'transform' : 'rotate(360deg)' }
+    },
+    ping : {
+      '0%' : { 'transform' : 'scale(0.1, 0.1)' },
+      '50%' : { 'opacity' : '1.0' },
+      '100%' : { 'transform' : 'scale(1.2, 1.2)' }
     }
   },
     animation: {
